207d470049f1bc69fe1d3783a571087aad678ee0
Afterimages <zy13682943183@163.com>
2024-12-30 14:54:47 +0800

    day19

--- javatest/LCDay18.java
+++ javatest/LCDay18.java
@@ -1,14 +1,77 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LCDay18 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] ans = solution.twoSum(new int[]{2,7,11,15},9);
+        // int n = 7;
+        // boolean ans = isHappy(n);
+        int[] nums= {0,1,2,4,5,7};
+        // int k = 3;
+        List<String> ans = summaryRanges(nums);
         System.err.println(ans);
     }
+    public static List<String> summaryRanges(int[] nums) {
+        List<String> ans = new ArrayList<>();
+        int length = nums.length;
+        int i = 0;
+        StringBuffer str = new StringBuffer();
+        if (length == 0) return ans; // 处理空数组的情况
+        str.append(nums[i]);
+        while (i < length) {
+            int start = nums[i];
+            while (i < length - 1 && nums[i + 1] == nums[i] + 1) {
+                i++;
+            }
+            if (start != nums[i]) {
+                str.append("->").append(nums[i]);
+            }
+            ans.add(str.toString());
+            str = new StringBuffer();
+            i++;
+            if (i < length) {
+                str.append(nums[i]);
+            }
+        }
+        return ans;
+    }
+    // 存在重复元素:用哈希表记录，如果出现重复元素，返回true，否则返回false
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int length = nums.length;
+        for(int i = 0;i<length;i++){
+            if((map.containsKey(nums[i]))&&(i-map.get(nums[i])<=k)){
+                return true;
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return false;
+    }
+
+    // 快乐数:我觉得考点是运用整除和取模的知识，注意不要搞错先后顺序
+    public static boolean isHappy(int n) {
+        Map<Integer,Integer> map = new HashMap<>();
+        while(n!=1){
+            map.put(n,1);
+            int sum = 0;
+            while(n!=0){
+                sum+=(n%10)*(n%10);
+                n/=10;
+            }
+            if(!map.containsKey(sum)){
+                n = sum;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
 }
-class Solution {
+class Solution1 {
     // 两数之和我记得是用双指针。但这里刷题的时候在哈希表题库下，所以用哈希表
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();//数字：索引
--- javatest/leetcodeDay16.java
+++ javatest/leetcodeDay16.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class leetcodeDay16 {
     public String minWindow(String S, String t) {
