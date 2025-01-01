40d1e68fa9ec97e38d18debd425598e28296681d
Afterimages <zy13682943183@163.com>
2025-01-01 20:52:38 +0800

    简单题

--- javatest/LCDay18.java
+++ javatest/LCDay18.java
@@ -4,12 +4,25 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 class ListNode {
     int val;
     ListNode next;
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
 
 public class LCDay18 {
@@ -20,6 +33,102 @@ public class LCDay18 {
         ListNode head = new ListNode(0)
         boolean ans = mergeTwoLists(list1, list2);
         System.err.println(ans);
+    }
+
+    public void moveZeroes(int[] nums) {
+        
+    }
+
+    // 将有序数组转换为二叉搜索树:递归，注意递归的边界条件
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length-1);
+    }
+    public TreeNode helper(int[] nums,int left,int right){
+        if(left>right){
+            return null;
+        }
+        int mid = (left+right)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums,left,mid-1);
+        root.right = helper(nums,mid+1,right);
+        return root;
+    }
+
+    // 二叉树的直径:递归，注意递归的边界条件
+    static int max_d = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return max_d-1;
+    }
+    public static int depth(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        int L =depth(node.left);
+        int R = depth(node.right);
+        max_d = Math.max(max_d,L+R+1);
+        return Math.max(L,R)+1;
+    }
+    // 对称二叉树:递归，注意递归的边界条件
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left,root.right);
+    }
+    public static boolean check(TreeNode p,TreeNode q){
+        if(p==null&&q==null){
+            return true;
+        }
+        if(p==null||q==null){
+            return false;
+        }
+        if(p.val==q.val&&check(p.left,q.right)&&check(p.right,q.left)){
+            return true;
+        }
+        return false;
+    }
+
+    //翻转二叉树:递归，注意递归的边界条件
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode newLeft = invertTree(root.left);
+        TreeNode newRight = invertTree(root.right);
+        root.left = newRight;
+        root.right = newLeft;
+        return root;
+    }
+
+    // 判断两个二叉树是否相同:递归，单纯的递归，深度优先的逻辑比较简单，但是广度有限可以使用对列表示，就很优雅，所以这里使用广度优先（深度广度复杂度都一样）,注意递归的边界条件
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> tmpQueue = new LinkedList<TreeNode>();
+        tmpQueue.offer(p);
+        tmpQueue.offer(q);
+        while(!tmpQueue.isEmpty()){
+            p = tmpQueue.poll();
+            q = tmpQueue.poll();
+            if(p==null&&q==null){
+                continue;
+            }
+            if(p==null || q==null||p.val!=q.val){
+                return false;
+            }
+            tmpQueue.offer(p.left);
+            tmpQueue.offer(q.left);
+
+            tmpQueue.offer(p.right);
+            tmpQueue.offer(q.right);
+        }
+        return true;
+    }
+
+    // 二叉树的最大深度:递归，注意递归的边界条件
+    public int maxDepth(TreeNode root) {
+        if(root ==null){
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return Math.max(leftHeight,rightHeight)+1;
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
